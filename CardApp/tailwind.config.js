b54ff78856819a9099719be6a22b79c6d3e1a43c
Muhammed Semih BİLİŞ <91787565+semihbilis@users.noreply.github.com>
2022-08-17 17:33:42 +0300

    NPM, Webpack Setup

---
+++ CardApp/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['../Pages/**/*.cshtml', '../Areas/**/*.cshtml'],
+    theme: {
+        extend: {
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms')
+    ]
+}
